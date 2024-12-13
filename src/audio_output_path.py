c004e22cbb18ccbd3514fc61ac9e18fac3fa8979
Andreas Stassivik <andreas@stassi.net>
2024-12-13 15:33:49 -0800

    `audio_file_extension` integration

--- src/audio_output_path.py
+++ src/audio_output_path.py
@@ -1,21 +1,13 @@
 import os
+from .audio_file_extension import audio_file_extension
 
 
 def audio_output_path(audio_codec: str, input_file: str) -> str:
-    file_extension = {
-        'aac': 'm4a',
-        'ac3': 'ac3',
-        'mp3': 'mp3',
-        'opus': 'opus',
-    }.get(audio_codec)
-
-    if not file_extension:
-        raise ValueError(f'Unsupported audio codec: {audio_codec}')
-
     base_name = os.path.splitext(os.path.basename(input_file))[0]
-
     cache_dir = os.path.join('cache', base_name)
-
     os.makedirs(cache_dir, exist_ok=True)
 
-    return os.path.join(cache_dir, f'{base_name}.{file_extension}')
+    return os.path.join(
+        cache_dir,
+        f'{base_name}.{audio_file_extension(audio_codec)}'
+    )
