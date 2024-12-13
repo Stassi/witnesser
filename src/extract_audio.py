1bacf27f57d3c0e27915cbdfa7fedd49aa303bf4
Andreas Stassivik <andreas@stassi.net>
2024-12-13 01:57:50 -0800

    `save_video_probe` integration

--- src/extract_audio.py
+++ src/extract_audio.py
@@ -1,37 +1,10 @@
 import ffmpeg
-import json
-import os
 from .audio_output_path import audio_output_path
-from typing import TYPE_CHECKING
+from .save_video_probe import save_video_probe
 
-
-if TYPE_CHECKING:
-    from _typeshed import SupportsWrite
 
 def extract_audio(video_file: str):
-    probed_video = ffmpeg.probe(video_file)
-
-    base_name = os.path.splitext(os.path.basename(video_file))[0]
-    cache_dir = os.path.join("cache", base_name)
-    os.makedirs(cache_dir, exist_ok=True)
-
-    f: SupportsWrite[str]
-    with open(
-        os.path.join(
-            cache_dir,
-            f"{base_name}.video.json"
-        ),
-        'w',
-        encoding='utf-8'
-    ) as f:
-        json.dump(
-            probed_video,
-            f,
-            ensure_ascii=False,
-            indent=4,
-            sort_keys=True
-        )
-        f.write('\n')
+    probed_video = save_video_probe(video_file)
 
     audio_codec = next(
         (stream['codec_name'] for stream in probed_video['streams'] if stream['codec_type'] == 'audio'),
