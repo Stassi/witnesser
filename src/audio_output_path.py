56a9edb0ccc84fbce42506d24ac59a11d2b28e19
Andreas Stassivik <andreas@stassi.net>
2024-12-13 15:35:41 -0800

    explicit `audio_output_path` type annotations

--- src/audio_output_path.py
+++ src/audio_output_path.py
@@ -3,8 +3,9 @@ from .audio_file_extension import audio_file_extension
 
 
 def audio_output_path(audio_codec: str, input_file: str) -> str:
-    base_name = os.path.splitext(os.path.basename(input_file))[0]
-    cache_dir = os.path.join('cache', base_name)
+    base_name: str = os.path.splitext(os.path.basename(input_file))[0]
+    cache_dir: str = os.path.join('cache', base_name)
+
     os.makedirs(cache_dir, exist_ok=True)
 
     return os.path.join(
