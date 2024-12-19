ca8f72e0baa6dc35aa56a9c934393b2f002ca075
Andreas Stassivik <andreas@stassi.net>
2024-12-18 23:11:54 -0800

    `save_video_probe` returns `SaveVideoProbe` dataclass including `hash_digest`

--- src/save_video_probe.py
+++ src/save_video_probe.py
@@ -1,17 +1,35 @@
-import ffmpeg
 import json
 import os
+from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+import ffmpeg
+
+from .ffmpeg_probe import FFMpegProbe
+from .hash_digest import hash_digest
 
 if TYPE_CHECKING:
     from _typeshed import SupportsWrite
 
-def save_video_probe(video_file: str):
-    probed_video = ffmpeg.probe(video_file)
 
-    base_name: str = os.path.splitext(os.path.basename(video_file))[0]
-    cache_dir: str = os.path.join('cache', base_name)
+@dataclass
+class SaveVideoProbe:
+    hash_digest: str
+    probed_video: FFMpegProbe
+
+
+def save_video_probe(video_file: str) -> SaveVideoProbe:
+    probed_video: FFMpegProbe = ffmpeg.probe(video_file)
+
+    json_str: str = json.dumps(
+        ensure_ascii=False,
+        indent=4,
+        obj=probed_video,
+        sort_keys=True
+    ) + '\n'
+
+    digest: str = hash_digest(json_str)
+    cache_dir: str = os.path.join('cache', digest)
 
     os.makedirs(cache_dir, exist_ok=True)
 
@@ -19,18 +37,14 @@ def save_video_probe(video_file: str):
         encoding='utf-8',
         file=os.path.join(
             cache_dir,
-            f'{base_name}.av.json'
+            f'{os.path.splitext(os.path.basename(video_file))[0]}.av.json'
         ),
         mode='w',
     ) as f:
         f: SupportsWrite[str]
-        json.dump(
-            probed_video,
-            f,
-            ensure_ascii=False,
-            indent=4,
-            sort_keys=True
-        )
-        f.write('\n')
+        f.write(json_str)
 
-    return probed_video
+    return SaveVideoProbe(
+        hash_digest=digest,
+        probed_video=probed_video,
+    )
