0e51e4ecaaf3f31b38da84e65e43d2f19b2dd267
Andreas Stassivik <andreas@stassi.net>
2024-12-18 23:09:19 -0800

    `audio_output_path` ternary parameters `hash_digest` & `video_file` extended from unary `audio_codec`

--- src/audio_output_path.py
+++ src/audio_output_path.py
@@ -1,10 +1,17 @@
 import os
+
 from .audio_file_extension import audio_file_extension
 
 
-def audio_output_path(audio_codec: str, input_file: str) -> str:
-    base_name: str = os.path.splitext(os.path.basename(input_file))[0]
-    cache_dir: str = os.path.join('cache', base_name)
+def audio_output_path(
+    audio_codec: str,
+    hash_digest: str,
+    video_file: str
+) -> str:
+    base_name: str = os.path.splitext(
+        os.path.basename(video_file)
+    )[0]
+    cache_dir: str = os.path.join('cache', hash_digest)
 
     os.makedirs(cache_dir, exist_ok=True)
 
